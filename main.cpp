c98c4a742300346d9f79f3793e0f701b641dff0a
rjosodtssp <zhangzipan1@gmail.com>
2018-03-17 10:47:49 +0800

    :zap: 修复先启动程序后启动lockfile机制的缺陷

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QClipper w;
-    w.show();
     QLockFile lock("lock.lck");
     if(!lock.tryLock())
     {
         QMessageBox::warning(&w,"警告！","QClipper已经运行！");
         return 0;
     }
+    QClipper w;
+    w.show();
     return a.exec();
 }
