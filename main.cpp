312ae1a57214ffe06518ca9b68901136c69ee592
rjosodtssp <zhangzipan1@gmail.com>
2018-05-12 13:51:38 +0800

    :memo: 添加演示gif

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setStyle("Fusion");
     QLockFile lock("lock.lck");
     if(!lock.tryLock())
     {
-        QMessageBox::warning(&w,"警告！","QClipper已经运行！");
+        QMessageBox::warning(0,"警告！","QClipper已经运行！");
         return 0;
     }
     QClipper w;
